e3d1436154acf37c93564e761f592b191ae8ce40
JackGoldstein <jgoldstein3@ufl.edu>
2022-03-14 18:29:55 -0400

    Added subtraction ability

--- Addition.java
+++ Addition.java
@@ -4,9 +4,16 @@ public class Addition {
     public static void main(String[] args) {
         int operand1 = 0;
         int operand2 = 0;
+        int userInput = 0;
         Scanner scnr = new Scanner(System.in);
 
-        System.out.println("Enter two operands to add");
+        System.out.println("Would you like to add or subtract?");
+        System.out.println("1: Add");
+        System.out.println("2: Subtract");
+        userInput = scnr.nextInt();
+
+
+        System.out.println("Enter two operands to add/subtract");
 
         System.out.println("Enter operand 1:");
         operand1 = scnr.nextInt();
@@ -14,7 +21,12 @@ public class Addition {
         System.out.println("Enter operand 2:");
         operand2 = scnr.nextInt();
 
-        System.out.println("The sum is " + (operand1 + operand2));
+        if (userInput == 1) {
+            System.out.println("The sum is " + (operand1 + operand2));
+        } else if (userInput == 2) {
+            System.out.println("The difference is " + (operand1 - operand2));
+        }
+
 
 
     }
